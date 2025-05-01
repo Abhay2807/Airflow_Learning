53bebb29c70dde14453a21d50969195fb7435900
ABHAY SINGH <kushomaxe@gmail.com>
2025-05-01 22:42:30 +0530

    Create Example-02-Child_2.py

---
+++ DAG/Operators/TriggerDagOperator/Example-02-Child_2.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime, timedelta
+from airflow.utils.log.logging_mixin import LoggingMixin
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'retries': 0,
+    'retry_delay': timedelta(minutes=1)
+}
+
+# Python function to read the message passed from the parent DAG
+def read_message(**kwargs):
+    conf = kwargs['dag_run'].conf                       # Retrieve config
+    log = LoggingMixin().log                            # Access logger
+    log.info(f"Received message: {conf.get('msg')}")    # Log the message
+
+# Define Child DAG 2
+with DAG(
+    dag_id='Child_Dag_v2',                              # Unique ID for child DAG
+    description="This is Child Dag Two",                # Description
+    start_date=datetime(2025, 5, 1),                    # Start date
+    default_args=default_args,                          # Default arguments
+    schedule=None,                                      # Triggered only externally
+    catchup=False
+) as dag:
+
+    # PythonOperator to execute read_message function
+    read_msg = PythonOperator(
+        task_id='read_msg',
+        python_callable=read_message
+    )
